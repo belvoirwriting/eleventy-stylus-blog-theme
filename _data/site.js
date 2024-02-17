79d88b2c05992046a57e2080a51fcc7d248143d3
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2024-02-17 10:35:40 +0000

    Update site.js
    changed description

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
   meta: {
     title: "Vale of Belvoir Writing Group",
-    description: "A group for writers meeting Bottesford every month.",
+    description: "A group for writers meeting in Bottesford every month.",
     lang: "en",
-    siteUrl: "https://example.com/",
+    siteUrl: "https://belvoirwriting.co.uk/",
   },
   feed: { // used in feed.xml.njk
-    subtitle: "Lorem ipsum dolor sit amet consecuteor",
+    subtitle: "a writers group for Bottesford and the whole Vale of Belvoir",
     filename: "atom.xml",
     path: "/atom.xml",
     id: "https://example.com/",
-    authorName: "John Doe",
-    authorEmail: "johndoe@example.com"
+    authorName: "Vale of Belvoir Writring Group",
+    authorEmail: "words@belvoirwriting.co.uk"
   },
   hero: { // used in hero section of main page ie. index.html.njk
     title: "Welcome to the Vale of Belvoir Writing Group",
-    description: "meeting on the second Tuesday of every month."
+    description: "Meeting in Bottesford every second Tuesday of the month at 8pm in the Old School next to the library. Find your voice as a writer, feel confident and encouraged, and enjoy a supportive space to try out new ideas."
   }
 }
